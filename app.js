c8f71b1228674484c8c7cc8734048767942557f2
Prasanthk06 <prasanth.k@bcah.christuniversity.in>
2025-02-22 09:50:58 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import mysql from 'mysql2/promise';
-
-const conn = await mysql.createConnection('mysql://root:Prashanth5319@localhost:3306/lms')
-conn.connect();
-const [result] = await conn.execute("SELECT * FROM student");
-console.log(result);
-
-
